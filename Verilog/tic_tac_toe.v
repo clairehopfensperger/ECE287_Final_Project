45a86e91197747e07d729a032d63cbf6305ee337
Claire Hopfensperger <99447241+clairehopfensperger@users.noreply.github.com>
2022-11-14 17:49:23 -0500

    created a grid plan in comments

--- Verilog/tic_tac_toe.v
+++ Verilog/tic_tac_toe.v
@@ -1,5 +1,24 @@
 // top level module for project
-// literally nothing yet
+// literally nothing yet 
 
-module tic_tac_toe()
+module tic_tac_toe(clk, rst);
+	input clk, rst;
+
+	// Grid:
+	//       1    2    3
+	//    |----|----|----|
+	//    |    |    |    |
+	//  A | A1 | A2 | A3 |
+	//    |    |    |    |
+	//    -----|----|-----
+	//    |    |    |    |
+	//  B | B1 | B2 | B3 |
+	//    |    |    |    |
+	//    -----|----|-----
+	//    |    |    |    |
+	//  C | C1 | C2 | C3 |
+	//    |    |    |    |
+	//    |----|----|----|
+
+
 endmodule
