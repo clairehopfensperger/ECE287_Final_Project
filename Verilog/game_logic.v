43cd793fd23b7f44a59df6385648c5c81e370edd
Claire Hopfensperger <99447241+clairehopfensperger@users.noreply.github.com>
2022-11-14 17:48:44 -0500

    updated file - doesn't compile yet

--- Verilog/game_logic.v
+++ Verilog/game_logic.v
@@ -1,14 +1,15 @@
 // Edit 11/14/22 Claire Hopfensperger
+// DOES NOT COMPILE LIKE THIS - sorry i ran out of time to debug
 
 // This module implements the FSM that works the turn system
 
 module game_logic(clk, rst, move, outcome);
 	input clk, rst;
 	input [3:0]move; //9 grid squares
-	output reg [1:0]outcome; //win, lose, tie
+	output reg [1:0]outcome; //in_progress, win, lose, tie
 	
 	reg [1:0]TURN;
-	reg end;
+	reg end_game;
 	
 	reg [2:0]S;
 	reg [2:0]NS;
@@ -52,11 +53,11 @@ module game_logic(clk, rst, move, outcome);
 					NS = USER_2;
 			CHECK:
 			begin
-				if (end == 1'b0 && turn == 2'b01)
+				if (end_game == 1'b0 && turn == 2'b01)
 					NS = USER_1;
-				else if (end == 1'b0 && turn == 2'b10)
+				else if (end_game == 1'b0 && turn == 2'b10)
 					NS = USER_2;
-				else if (end == 1'b1)
+				else if (end_game == 1'b1)
 					NS = END;
 				else
 					NS = CHECK_1;
