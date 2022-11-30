f906d97d426b92190ec880b2f37903eaed8df485
Claire Hopfensperger <99447241+clairehopfensperger@users.noreply.github.com>
2022-11-29 19:07:29 -0500

    changed parameter definitions
    still doesn't work, pretty much just getting ideas down

--- Verilog/game_logic.v
+++ Verilog/game_logic.v
@@ -1,5 +1,4 @@
 // Edit 11/14/22 Claire Hopfensperger
-// DOES NOT COMPILE LIKE THIS - sorry i ran out of time to debug
 
 // This module implements the FSM that works the turn system
 
@@ -8,15 +7,43 @@ module game_logic(clk, rst, move, outcome);
 	input [3:0]move; //9 grid squares
 	output reg [1:0]outcome; //in_progress, win, lose, tie
 	
-	reg [1:0]TURN;
+	// Grid:
+	//       1    2    3
+	//    |----|----|----|
+	//    |    |    |    |
+	//  A | A1 | A2 | A3 |
+	//    |    |    |    |
+	//    -----|----|-----
+	//    |    |    |    |
+	//  B | B1 | B2 | B3 |
+	//    |    |    |    |
+	//    -----|----|-----
+	//    |    |    |    |
+	//  C | C1 | C2 | C3 |
+	//    |    |    |    |
+	//    |----|----|----|
+	
+	parameter A1 = 4'd1,
+				 A2 = 4'd2,
+				 A3 = 4'd3,
+				 B1 = 4'd4,
+				 B2 = 4'd5,
+				 B3 = 4'd6,
+				 C1 = 4'd7,
+				 C2 = 4'd8,
+				 C3 = 4'd9;
+	
+	reg [1:0]turn;
 	reg end_game;
+	
+	move_logic my_move_logic(clk, rst, move, user, valid, outcome);
 	
 	reg [2:0]S;
 	reg [2:0]NS;
 	parameter START = 3'd0,
 				 USER_1 = 3'd1,
 				 USER_2 = 3'd2,
-				 CHECK = 3'd3, //will have to implement another module for checking move validity
+				 CHECK = 3'd3, 
 				 END = 3'd4,
 				 ERROR = 3'hF;
 				 
@@ -41,7 +68,7 @@ module game_logic(clk, rst, move, outcome);
 			USER_1:
 			begin
 				if (turn == 2'b00)
-					NS = CHECK_1;
+					NS = CHECK;
 				else
 					NS = USER_1;
 			end
@@ -60,7 +87,7 @@ module game_logic(clk, rst, move, outcome);
 				else if (end_game == 1'b1)
 					NS = END;
 				else
-					NS = CHECK_1;
+					NS = CHECK;
 			end
 			END: NS = END;
 			default: NS = ERROR;
@@ -71,6 +98,7 @@ module game_logic(clk, rst, move, outcome);
 	begin
 		if (rst == 1'b0)
 		begin
+			turn <= 2'b01;
 		end
 		else
 		begin
